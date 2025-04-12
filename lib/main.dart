4dc27489fab50f1aea0e449355b4bf6e39da9aeb
Daniel Brown Rodrigues Mingozzi dos Passos <daniel_academico1998@hotmail.com>
2025-04-12 13:02:40 -0300

    initial

---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:client_control/pages/client_types_page.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'pages/clients_page.dart';
+import "models/clients.dart";
+
+void main() {
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => Clients(clients: []),
+      child: const MyApp(),
+    ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Controle de clientes',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.indigo,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const ClientsPage(title: 'Clientes'),
+        '/tipos': (context) => const ClientTypesPage(title: 'Tipos de cliente'),
+      },
+    );
+  }
+}
